03ea336dedde1b4bcec822ff515cb9ba1b0f4da7
ASUS <adi2001rangerbatman>
2023-10-25 00:16:41 +0530

    Map Done

--- lib/features/App/login/data_list.dart
+++ lib/features/App/login/data_list.dart
@@ -12,50 +12,61 @@ class MapSample extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapSample> {
-  static const LatLng _pGooglePlex = LatLng(37.4223, -122.0848);
-
-  // static const CameraPosition _kGooglePlex = CameraPosition(
-  //   target: LatLng(37.42796133580664, -122.085749655962),
-  //   zoom: 14.4746,
-  // );
-
-  // static const CameraPosition _kLake = CameraPosition(
-  //     bearing: 192.8334901395799,
-  //     target: LatLng(37.43296265331129, -122.08832357078792),
-  //     tilt: 59.440717697143555,
-  //     zoom: 19.151926040649414);
+  late GoogleMapController mapController;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: const CameraPosition(
-          target: LatLng(7.8731, 80.7718),
-          zoom: 20,
+          target: LatLng(6.9271, 79.8612),
+          zoom: 10,
         ),
         markers: {
           const Marker(
             markerId: MarkerId("Temple Of the Tooth"),
             position: LatLng(7.293627, 80.641350),
             icon: BitmapDescriptor.defaultMarker,
+            infoWindow: InfoWindow(
+              title: 'Temple Of the Tooth',
+              snippet: 'Central Province'
+            ),
           ),
           const Marker(
             markerId: MarkerId("Sigiriya"),
             position: LatLng(7.956944, 80.759720),
             icon: BitmapDescriptor.defaultMarker,
+            infoWindow: InfoWindow(
+              title: 'Sigiriya',
+              snippet: 'Central Province'
+            ),
           ),
           const Marker(
             markerId: MarkerId("Watadageya"),
             position: LatLng(8.1561, 80.9962),
             icon: BitmapDescriptor.defaultMarker,
+            infoWindow: InfoWindow(
+              title: 'Watadageya',
+              snippet: 'North Central',
+            ),
           )
         },
       ),
     );
   }
+  
 
   // Future<void> _goToTheLake() async {
   //   final GoogleMapController controller = await _controller.future;
   //   await controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   // }
 }
+class  extends StatelessWidget {
+  const ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return const MaterialApp(
-      home:MapSample(),
+      home:home(),
       debugShowCheckedModeBanner: false,
     );
   }
