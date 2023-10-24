ff32426a3ad8cba2f3781bbe0fa314a5500e7468
ASUS <adi2001rangerbatman>
2023-10-24 12:51:24 +0530

    Home page Updated

--- lib/features/App/login/home.dart
+++ lib/features/App/login/home.dart
@@ -18,46 +18,92 @@ class _State extends State<home> {
   Widget build(BuildContext context) {
     mediasize = MediaQuery.of(context).size;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(50.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  "Travelers",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 30.0,
-              ),
-              TextField(
-                style:TextStyle(
-                    color: Colors.black,
-                  fontWeight: FontWeight.bold
-                ),
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor:Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  hintText: "Ex:Sigiriya ",
-                  prefixIcon: Icon(Icons.search),
-                  prefixIconColor: Colors.black
-                ),
+      body: Column(
 
-              )
-            ],
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 40.0, 20.0, 0),
+
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "Travelers",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextField(
+                    style:TextStyle(
+                        color: Colors.black,
+                      fontWeight: FontWeight.bold
+                    ),
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor:Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      hintText: "Ex:Sigiriya ",
+                      prefixIcon: Icon(Icons.search),
+                      prefixIconColor: Colors.black
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Center(
+                      child: Text("Most Visited Palces",style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22.0,
+                      )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30.0,
+                  ),
+                    ],
+                  )
+              ),
+            ),
+
+          Padding(padding: EdgeInsets.fromLTRB(5.0,0,5.0,0),
+            child: Container(
+              height:140,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  buildCard(),
+                  SizedBox(width: 12,),
+                  buildCard(),
+                  SizedBox(width: 12,),
+                  buildCard(),
+                  SizedBox(width: 12,),
+                  buildCard(),
+                  SizedBox(width: 12,),
+                ],
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              child: SizedBox(
+                  child: Text("Place Nearby",style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22.0,
+                  ),)),
+            ),
+          ),
+        ],
       ),
     );
 
@@ -66,18 +112,13 @@ class _State extends State<home> {
           context, MaterialPageRoute(builder: (context) => const Login())));
     }
   }
+  Widget buildCard()=> Container(
+    width: 200,
+    decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(20),
+      border: Border.all(color: Colors.black)
+    ),
+  );
 }
-// class CustomerSearchDelegate extends  SearchDelegate{
-//   @override
-//   List<Widget> buildActions(BuildContext context){}
-//
-//   @override
-//   List<Widget> buildActions(BuildContext context){}
-//
-//   @override
-//   List<Widget> buildActions(BuildContext context){}
-//
-//   @override
-//   List<Widget> buildActions(BuildContext context){}
 
-// }
